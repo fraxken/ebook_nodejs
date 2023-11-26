fbc94ebf19b1f0916170cb48047aed48e248fb02
Nicolas Hallaert <39910164+Rossb0b@users.noreply.github.com>
2023-11-26 01:03:11 +0100

    Improve pdf (#27)
    * chore(): jsdom, marked, @myunisoft/html-to-pdf, @rossbob/image-to-base64
    
    * refactor(): using <h1>, as marked doesn't seem to deal with it properly
    
    * feature(script/generatePdf): summary in first page, use of base64 for images, add perf
    
    * refactor(scripts/generatePdf): parse md before dom serialization
    
    * chore(): marked-alert
    
    * feature(script/generatePdf): use marked-alert

--- scripts/generatePdf.js
+++ scripts/generatePdf.js
@@ -1,13 +1,18 @@
 // Import Node.js Dependencies
+import { Readable } from "node:stream";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 import fs from "node:fs";
-import util from "node:util";
+import { readFile, writeFile } from "node:fs/promises";
+import { parseArgs } from 'node:util';
 
 // Import Third-party Dependencies
-import MarkdownPDF from "markdown-pdf";
 import { walkSync } from "@nodesecure/fs-walk";
-import through from "through2";
+import { generatePDF, initBrowser, terminateBrowser } from "@myunisoft/html-to-pdf";
+import { toBase64 } from "@rossbob/image-to-base64";
+import { marked } from "marked";
+import markedAlert from "marked-alert";
+import { JSDOM } from "jsdom";
 
 // CONSTANTS
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
@@ -16,7 +21,11 @@ const kPageBreak = `\n\n<div style="page-break-before: always;"></div>\n\n`;
 const kAvailableLangs = new Set(["fr", "en"]);
 const kNumericCollator = new Intl.Collator(undefined, { numeric: true, sensitivity: "base" });
 
-const args = util.parseArgs({
+marked.use(markedAlert());
+
+const start = performance.now();
+
+const args = parseArgs({
     options: {
         lang: {
             type: "string",
@@ -43,14 +52,51 @@ const files = [...syncIterator]
     .reverse()
     .sort(kNumericCollator.compare);
 
-function preProcessMd() {
-    return through(function(data, enc, cb) {
-        cb(null, Buffer.concat([data, Buffer.from(kPageBreak)]));
-    });
+async function main(pdfOptions = {
+    margin: {
+        top: "20px",
+        bottom: "20px",
+        left: "40px",
+        right: "40px"
+    },
+    scale: 1.2
+}) {
+    let stringHTML = "";
+
+    const readme = files.find((fileName) => fileName.endsWith("README.md"));
+    stringHTML += await readFile(readme) + kPageBreak;
+
+    for (const file of files) {
+        if (file.endsWith("README.md")) {
+            continue;
+        }
+
+        stringHTML += await readFile(file) + kPageBreak;
+    }
+
+    const dom = new JSDOM(marked.parse(stringHTML), { contentType: "text/html" });
+
+    const regexPath = /^https?:\/\//i;
+    const images = dom.window.document.getElementsByTagName("img");
+
+    for (const image of images) {
+        if (!regexPath.test(image.src)) {  
+            const splitted = image.src.split("./");
+            const base64 = await toBase64({ path: path.join(__dirname, "..", splitted[splitted.length - 1]) });
+            image.src = `data:image/png;base64,${base64.base64}`;
+        }
+    }
+
+    const browser = await initBrowser();
+
+    try {
+        await writeFile(path.join(kOutDir, `${lang}.pdf`), Readable.from(generatePDF(browser, [{ content: dom.serialize() }], pdfOptions)))
+    }
+    finally {
+        await terminateBrowser(browser);
+    }
 }
 
-MarkdownPDF({ preProcessMd, remarkable: { html: true, breaks: true } })
-    .concat.from(files)
-    .to(path.join(kOutDir, `${lang}.pdf`), () => {
-        console.log("PDF successfully created!");
-    });
+main()
+    .then(() => console.log("PDF successfully created!", performance.now() - start))
+    .catch((error) => console.error(error));
